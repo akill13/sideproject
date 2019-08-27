c0e590932e89424e5eaf89285de7dcbd5e540559
Abdul Halimi <abdulhalimi13@yahoo.com>
2019-08-27 19:40:27 -0400

    Organized front end into separate modules

---
+++ test/client/src/app/shared/car/component/car-parent/car-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-parent',
+  templateUrl: './car-parent.component.html',
+  styleUrls: ['./car-parent.component.scss']
+})
+export class CarParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
